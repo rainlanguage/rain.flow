fd0ae2b206c6cb3e6f4b6f5ec238d477cf3059fc
Erik Dubovyk <erik.dubovyk@cyber-bee.co.uk>
2024-08-05 12:01:10 +0300

    Ported test for ERC721 to ERC1155 token flow.

---
+++ test/concrete/Flow/FlowERC721ToERC1155Test.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: CAL
+pragma solidity ^0.8.18;
+
+import {FlowBaseTest} from "test/abstract/FlowBaseTest.sol";
+import {IFlowV5, ERC20Transfer, ERC721Transfer, ERC1155Transfer} from "src/interface/unstable/IFlowV5.sol";
+import {EvaluableV2} from "rain.interpreter.interface/lib/caller/LibEvaluable.sol";
+import {IERC721} from "openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+import {IERC1155} from "openzeppelin-contracts/contracts/token/ERC1155/IERC1155.sol";
+import {REVERTING_MOCK_BYTECODE} from "test/abstract/TestConstants.sol";
+
+contract FlowERC721ToERC1155Test is FlowBaseTest {
+    function setUp() public {}
+
+    address internal immutable _iERC721;
+    address internal immutable _iERC1155;
+
+    constructor() {
+        vm.pauseGasMetering();
+        _iERC721 = address(uint160(uint256(keccak256("erc721.test"))));
+        vm.etch(address(_iERC721), REVERTING_MOCK_BYTECODE);
+
+        _iERC1155 = address(uint160(uint256(keccak256("store.rain.test"))));
+        vm.etch(address(_iERC1155), REVERTING_MOCK_BYTECODE);
+        vm.resumeGasMetering();
+    }
+
+    function testFlowERC721ToERC1155(
+        address alice,
+        uint256 erc721InTokenId,
+        uint256 erc1155OutTokenId,
+        uint256 erc1155OutAmmount
+    ) external {
+        vm.assume(alice != address(0));
+        vm.label(alice, "Alice");
+
+        (IFlowV5 flow, EvaluableV2 memory evaluable) = deployFlow();
+
+        ERC721Transfer[] memory erc721Transfers = new ERC721Transfer[](1);
+        erc721Transfers[0] =
+            ERC721Transfer({token: address(_iERC721), from: alice, to: address(flow), id: erc721InTokenId});
+
+        ERC1155Transfer[] memory erc1155Transfers = new ERC1155Transfer[](1);
+        erc1155Transfers[0] = ERC1155Transfer({
+            token: address(_iERC1155),
+            from: address(flow),
+            to: alice,
+            id: erc1155OutTokenId,
+            amount: erc1155OutAmmount
+        });
+
+        vm.mockCall(
+            _iERC1155,
+            abi.encodeWithSelector(
+                bytes4(keccak256("safeTransferFrom(address,address,uint256,uint256,bytes)")),
+                flow,
+                alice,
+                erc1155OutTokenId,
+                erc1155OutAmmount,
+                ""
+            ),
+            abi.encode()
+        );
+        vm.expectCall(
+            _iERC1155,
+            abi.encodeWithSelector(
+                bytes4(keccak256("safeTransferFrom(address,address,uint256,uint256,bytes)")),
+                flow,
+                alice,
+                erc1155OutTokenId,
+                erc1155OutAmmount,
+                ""
+            )
+        );
+
+        vm.mockCall(
+            _iERC721,
+            abi.encodeWithSelector(
+                bytes4(keccak256("safeTransferFrom(address,address,uint256)")), alice, flow, erc721InTokenId
+            ),
+            abi.encode()
+        );
+
+        vm.expectCall(
+            _iERC721,
+            abi.encodeWithSelector(
+                bytes4(keccak256("safeTransferFrom(address,address,uint256)")), alice, flow, erc721InTokenId
+            )
+        );
+
+        vm.startPrank(alice);
+        performFlow(flow, evaluable, new ERC20Transfer[](0), erc721Transfers, erc1155Transfers);
+        vm.stopPrank();
+    }
+}
