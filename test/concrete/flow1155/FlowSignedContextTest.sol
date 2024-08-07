604adf9ed52d06cc0b258e064d8536ba840692f0
ninokeldishvili <ninokeldishvili@gmail.com>
2024-08-07 11:57:54 +0400

    Revert fuzzing

--- test/concrete/flow1155/FlowSignedContextTest.sol
+++ test/concrete/flow1155/FlowSignedContextTest.sol
@@ -22,6 +22,53 @@ contract FlowSignedContextTest is SignContextAbstractTest, FlowUtilsAbstractTest
         string memory uri,
         uint256[] memory context0,
         uint256[] memory context1,
+        uint256 fuzzedKeyAlice,
+        uint256 fuzzedKeyBob
+    ) public {
+        vm.assume(fuzzedKeyBob != fuzzedKeyAlice);
+        (IFlowERC1155V5 erc1155Flow, EvaluableV2 memory evaluable) = deployIFlowERC1155V5(uri);
+
+        // Ensure the fuzzed key is within the valid range for secp256k1
+        uint256 aliceKey = (fuzzedKeyAlice % (SECP256K1_ORDER - 1)) + 1;
+        uint256 bobKey = (fuzzedKeyBob % (SECP256K1_ORDER - 1)) + 1;
+
+        SignedContextV1[] memory signedContexts = new SignedContextV1[](2);
+        signedContexts[0] = signContext(aliceKey, context0);
+        signedContexts[1] = signContext(aliceKey, context1);
+
+        uint256[] memory stack = generateFlowERC1155Stack(
+            new ERC1155Transfer[](0),
+            new ERC721Transfer[](0),
+            new ERC20Transfer[](0),
+            new ERC1155SupplyChange[](0),
+            new ERC1155SupplyChange[](0)
+        );
+        interpreterEval2MockCall(stack, new uint256[](0));
+        erc1155Flow.flow(evaluable, new uint256[](0), signedContexts);
+
+        // With bad signature in second signed context
+        SignedContextV1[] memory signedContexts1 = new SignedContextV1[](2);
+        signedContexts[0] = signContext(aliceKey, context0);
+        signedContexts[1] = signContext(bobKey, context1);
+
+        uint256[] memory stack1 = generateFlowERC1155Stack(
+            new ERC1155Transfer[](0),
+            new ERC721Transfer[](0),
+            new ERC20Transfer[](0),
+            new ERC1155SupplyChange[](0),
+            new ERC1155SupplyChange[](0)
+        );
+        interpreterEval2MockCall(stack1, new uint256[](0));
+
+        vm.expectRevert(abi.encodeWithSelector(InvalidSignature.selector, 0));
+        erc1155Flow.flow(evaluable, new uint256[](0), signedContexts1);
+    }
+
+    /// Should validate a signed context
+    function testValidateSignedContexts(
+        string memory uri,
+        uint256[] memory context0,
+        uint256[] memory context1,
         uint256[] memory callerContext,
         uint256[] memory writes,
         uint256 fuzzedKeyAlice,
