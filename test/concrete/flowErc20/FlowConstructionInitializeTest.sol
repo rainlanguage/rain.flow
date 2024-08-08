2aa129861a5f75e9750e244e5abe8e2317af72bd
Erik Dubovyk <erik.dubovyk@cyber-bee.co.uk>
2024-08-08 19:13:49 +0300

    updated structure and naming

--- test/concrete/flowErc20/FlowConstructionInitializeTest.sol
+++ test/concrete/flowErc20/FlowConstructionInitializeTest.sol
@@ -2,25 +2,11 @@
 pragma solidity ^0.8.18;
 
 import {Vm} from "forge-std/Test.sol";
-
-import {InterpreterMockTest} from "test/abstract/InterpreterMockTest.sol";
-import {FlowERC20ConfigV2, IFlowERC20V5} from "src/interface/unstable/IFlowERC20V5.sol";
+import {FlowERC20ConfigV2} from "src/interface/unstable/IFlowERC20V5.sol";
 import {EvaluableConfigV3} from "rain.interpreter.interface/interface/IInterpreterCallerV2.sol";
-import {FlowERC20} from "src/concrete/erc20/FlowERC20.sol";
-import {CloneFactory} from "rain.factory/src/concrete/CloneFactory.sol";
-import {FlowUtilsAbstractTest} from "test/abstract/FlowUtilsAbstractTest.sol";
+import {FlowERC20Test} from "test/abstract/FlowERC20Test.sol";
 
-contract FlowErc20ConstructionInitializeTest is InterpreterMockTest, FlowUtilsAbstractTest {
-    CloneFactory internal immutable iCloneFactory;
-    IFlowERC20V5 internal immutable iFlowERC20Implementation;
-
-    constructor() {
-        vm.pauseGasMetering();
-        iCloneFactory = new CloneFactory();
-        iFlowERC20Implementation = new FlowERC20();
-        vm.resumeGasMetering();
-    }
-
+contract FlowConstructionInitializeTest is FlowERC20Test {
     function testFlowConstructionInitialize(address expression, bytes memory bytecode, uint256[] memory constants)
         external
     {
