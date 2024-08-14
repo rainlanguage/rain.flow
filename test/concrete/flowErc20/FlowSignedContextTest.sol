d22c2e1f5a2aecedfdf3eec03d4450023cb2b230
ninokeldishvili <ninokeldishvili@gmail.com>
2024-08-14 16:55:40 +0400

    Add testFlowERC20ValidateSignedContexts

--- test/concrete/flowErc20/FlowSignedContextTest.sol
+++ test/concrete/flowErc20/FlowSignedContextTest.sol
@@ -66,4 +66,49 @@ contract FlowSignedContextTest is FlowUtilsAbstractTest, FlowERC20Test {
         vm.expectRevert(abi.encodeWithSelector(InvalidSignature.selector, 1));
         erc20Flow.flow(evaluable, new uint256[](0), signedContexts1);
     }
+
+    /// Should validate a signed context
+    function testFlowERC20ValidateSignedContexts(
+        string memory name,
+        string memory symbol,
+        uint256[] memory context0,
+        uint256 fuzzedKeyAlice,
+        uint256 fuzzedKeyBob
+    ) public {
+        vm.assume(fuzzedKeyBob != fuzzedKeyAlice);
+        (IFlowERC20V5 erc20Flow, EvaluableV2 memory evaluable) = deployFlowERC20(name, symbol);
+
+        // Ensure the fuzzed key is within the valid range for secp256k1
+        uint256 aliceKey = (fuzzedKeyAlice % (SECP256K1_ORDER - 1)) + 1;
+        uint256 bobKey = (fuzzedKeyBob % (SECP256K1_ORDER - 1)) + 1;
+
+        SignedContextV1[] memory signedContext = new SignedContextV1[](1);
+        signedContext[0] = vm.signContext(aliceKey, aliceKey, context0);
+
+        uint256[] memory stack = generateFlowERC20Stack(
+            new ERC1155Transfer[](0),
+            new ERC721Transfer[](0),
+            new ERC20Transfer[](0),
+            new ERC20SupplyChange[](0),
+            new ERC20SupplyChange[](0)
+        );
+        interpreterEval2MockCall(stack, new uint256[](0));
+        erc20Flow.flow(evaluable, new uint256[](0), signedContext);
+
+        // With bad signature in second signed context
+        SignedContextV1[] memory signedContext1 = new SignedContextV1[](1);
+        signedContext1[0] = vm.signContext(aliceKey, bobKey, context0);
+
+        uint256[] memory stack1 = generateFlowERC20Stack(
+            new ERC1155Transfer[](0),
+            new ERC721Transfer[](0),
+            new ERC20Transfer[](0),
+            new ERC20SupplyChange[](0),
+            new ERC20SupplyChange[](0)
+        );
+        interpreterEval2MockCall(stack1, new uint256[](0));
+
+        vm.expectRevert(abi.encodeWithSelector(InvalidSignature.selector, 0));
+        erc20Flow.flow(evaluable, new uint256[](0), signedContext1);
+    }
 }
