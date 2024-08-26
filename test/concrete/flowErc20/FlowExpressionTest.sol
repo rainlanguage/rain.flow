6852a6407db65cb30fb513f08b9e12c73c1baf51
ninokeldishvili <ninokeldishvili@gmail.com>
2024-08-26 09:51:34 +0400

    Add testFlowERC20ShouldValidateContextFromEvent

--- test/concrete/flowErc20/FlowExpressionTest.sol
+++ test/concrete/flowErc20/FlowExpressionTest.sol
@@ -2,17 +2,15 @@
 pragma solidity ^0.8.18;
 
 import {Vm} from "forge-std/Test.sol";
-import {FlowBasicTest} from "test/abstract/FlowBasicTest.sol";
-import {
-    IFlowV5, FlowTransferV1, ERC20Transfer, ERC721Transfer, ERC1155Transfer
-} from "src/interface/unstable/IFlowV5.sol";
+import {IFlowERC20V5, ERC20SupplyChange} from "src/interface/unstable/IFlowERC20V5.sol";
+import {FlowTransferV1, ERC20Transfer, ERC721Transfer, ERC1155Transfer} from "src/interface/unstable/IFlowV5.sol";
 import {EvaluableV2} from "rain.interpreter.interface/lib/caller/LibEvaluable.sol";
-import {EvaluableConfigV3, SignedContextV1} from "rain.interpreter.interface/interface/IInterpreterCallerV2.sol";
-import {LibEvaluable} from "rain.interpreter.interface/lib/caller/LibEvaluable.sol";
+import {SignedContextV1} from "rain.interpreter.interface/interface/IInterpreterCallerV2.sol";
 import {LibUint256Matrix} from "rain.solmem/lib/LibUint256Matrix.sol";
 import {SignContextLib} from "test/lib/SignContextLib.sol";
 import {ContextBuilder} from "test/lib/ContextBuilder.sol";
 import {FlowERC20Test} from "../../abstract/FlowERC20Test.sol";
+import {FlowERC20IOV1} from "src/interface/unstable/IFlowERC20V5.sol";
 
 contract FlowExpressionTest is FlowERC20Test {
     using SignContextLib for Vm;
@@ -36,6 +34,64 @@ contract FlowExpressionTest is FlowERC20Test {
 
         for (uint256 i = 0; i < evaluables.length; i++) {
             assertEq(evaluables[i].expression, expressions[i]);
+        }
+    }
+
+    /**
+     * @dev Validates that the context emitted in the event matches the expected values.
+     */
+    function testFlowERC20ShouldValidateContextFromEvent(
+        uint256 fuzzedKeyAlice,
+        uint256[] memory fuzzedcallerContext0,
+        uint256[] memory fuzzedcallerContext1,
+        string memory name,
+        string memory symbol
+    ) public {
+        uint256[][] memory matrixCallerContext =
+            fuzzedcallerContext0.matrixFrom(fuzzedcallerContext1, fuzzedcallerContext0);
+
+        (IFlowERC20V5 flowErc20, EvaluableV2 memory evaluable) = deployFlowERC20(name, symbol);
+
+        {
+            uint256[] memory stack = generateFlowStack(
+                FlowERC20IOV1(
+                    new ERC20SupplyChange[](0),
+                    new ERC20SupplyChange[](0),
+                    FlowTransferV1(new ERC20Transfer[](0), new ERC721Transfer[](0), new ERC1155Transfer[](0))
+                )
+            );
+
+            interpreterEval2MockCall(stack, new uint256[](0));
+        }
+
+        SignedContextV1[] memory signedContext = new SignedContextV1[](matrixCallerContext.length);
+        {
+            // Ensure the fuzzed key is within the valid range for secp256k1
+            uint256 aliceKey = (fuzzedKeyAlice % (SECP256K1_ORDER - 1)) + 1;
+            for (uint256 i = 0; i < matrixCallerContext.length; i++) {
+                signedContext[i] = vm.signContext(aliceKey, aliceKey, matrixCallerContext[i]);
+            }
+
+            vm.recordLogs();
+            flowErc20.flow(evaluable, fuzzedcallerContext0, signedContext);
+        }
+
+        {
+            // Replace Flow contract's bytecode with ContextBuilder's bytecode
+            vm.etch(address(flowErc20), type(ContextBuilder).runtimeCode);
+            uint256[][] memory buildContextInput =
+                ContextBuilder(address(flowErc20)).buildContext(address(this), fuzzedcallerContext0, signedContext);
+
+            Vm.Log[] memory logs = vm.getRecordedLogs();
+            Vm.Log memory log = findEvent(logs, keccak256("Context(address,uint256[][])"));
+            (address sender, uint256[][] memory buildContextOutput) = abi.decode(log.data, (address, uint256[][]));
+
+            assertEq(sender, address(this), "wrong sender");
+            assertEq(
+                keccak256(abi.encode(buildContextInput)),
+                keccak256(abi.encode(buildContextOutput)),
+                "wrong compare  Context Input Output"
+            );
         }
     }
 }
