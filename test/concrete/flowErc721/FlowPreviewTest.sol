46ba63e8acaf143b34e7f3f3d6784e3e7cf2f051
Erik Dubovyk <erik.dubovyk@cyber-bee.co.uk>
2024-09-02 13:57:19 +0300

    testFlowERC721PreviewDefinedFlowIOForERC1155MultiElementArrays

---
+++ test/concrete/flowErc721/FlowPreviewTest.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: CAL
+pragma solidity =0.8.19;
+
+import {Vm} from "forge-std/Test.sol";
+import {EvaluableV2} from "rain.interpreter.interface/lib/caller/LibEvaluable.sol";
+import {REVERTING_MOCK_BYTECODE} from "test/abstract/TestConstants.sol";
+import {SignedContextV1} from "rain.interpreter.interface/interface/IInterpreterCallerV2.sol";
+import {LibEvaluable} from "rain.interpreter.interface/lib/caller/LibEvaluable.sol";
+import {FlowTransferV1, ERC20Transfer, ERC721Transfer, ERC1155Transfer} from "src/interface/unstable/IFlowV5.sol";
+import {FlowUtilsAbstractTest} from "test/abstract/FlowUtilsAbstractTest.sol";
+
+import {FlowERC721Test} from "test/abstract/FlowERC721Test.sol";
+import {IFlowERC721V5, ERC721SupplyChange, FlowERC721IOV1} from "../../../src/interface/unstable/IFlowERC721V5.sol";
+import {SignContextLib} from "test/lib/SignContextLib.sol";
+import {IERC20Upgradeable as IERC20} from
+    "openzeppelin-contracts-upgradeable/contracts/token/ERC20/IERC20Upgradeable.sol";
+import {IERC1155Upgradeable as IERC1155} from
+    "openzeppelin-contracts-upgradeable/contracts/token/ERC1155/IERC1155Upgradeable.sol";
+
+contract FlowPreviewTest is FlowERC721Test {
+    using LibEvaluable for EvaluableV2;
+
+    /**
+     * @dev Tests the preview of defined Flow IO for ERC1155
+     *      using multi-element arrays.
+     */
+    function testFlowERC721PreviewDefinedFlowIOForERC1155MultiElementArrays(
+        string memory symbol,
+        string memory baseURI,
+        address alice,
+        uint256 erc1155OutTokenIdA,
+        uint256 erc1155OutAmmountA,
+        uint256 erc1155InTokenIdB,
+        uint256 erc1155InAmmountB
+    ) external {
+        vm.assume(sentinel != erc1155OutTokenIdA);
+        vm.assume(sentinel != erc1155OutAmmountA);
+        vm.assume(sentinel != erc1155InTokenIdB);
+        vm.assume(sentinel != erc1155InAmmountB);
+        vm.label(alice, "alice");
+
+        (IFlowERC721V5 flow,) = deployFlowERC721({name: symbol, symbol: symbol, baseURI: baseURI});
+
+        ERC1155Transfer[] memory erc1155Transfers = new ERC1155Transfer[](4);
+
+        erc1155Transfers[0] = ERC1155Transfer({
+            token: address(iTokenA),
+            from: address(flow),
+            to: alice,
+            id: erc1155OutTokenIdA,
+            amount: erc1155OutAmmountA
+        });
+
+        erc1155Transfers[1] = ERC1155Transfer({
+            token: address(iTokenB),
+            from: address(flow),
+            to: alice,
+            id: erc1155InTokenIdB,
+            amount: erc1155InAmmountB
+        });
+
+        erc1155Transfers[2] = ERC1155Transfer({
+            token: address(iTokenA),
+            from: alice,
+            to: address(flow),
+            id: erc1155OutTokenIdA,
+            amount: erc1155OutAmmountA
+        });
+
+        erc1155Transfers[3] = ERC1155Transfer({
+            token: address(iTokenB),
+            from: alice,
+            to: address(flow),
+            id: erc1155InTokenIdB,
+            amount: erc1155InAmmountB
+        });
+
+        ERC721SupplyChange[] memory mints = new ERC721SupplyChange[](2);
+        mints[0] = ERC721SupplyChange({account: alice, id: 1});
+        mints[1] = ERC721SupplyChange({account: alice, id: 2});
+
+        ERC721SupplyChange[] memory burns = new ERC721SupplyChange[](1);
+        burns[0] = ERC721SupplyChange({account: alice, id: 2});
+
+        FlowERC721IOV1 memory flowERC721IO = FlowERC721IOV1(
+            mints, burns, FlowTransferV1(new ERC20Transfer[](0), new ERC721Transfer[](0), erc1155Transfers)
+        );
+
+        uint256[] memory stack = generateFlowStack(flowERC721IO);
+
+        assertEq(
+            keccak256(abi.encode(flowERC721IO)), keccak256(abi.encode(flow.stackToFlow(stack))), "wrong compare Structs"
+        );
+    }
+}
