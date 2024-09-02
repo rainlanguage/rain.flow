018b71c6f3aa82468f8d3eeef91cd5a070e1748b
David Meister <thedavidmeister@gmail.com>
2024-09-02 16:11:46 +0400

    Update test/concrete/flowBasic/FlowExpressionTest.sol

--- test/concrete/flowBasic/FlowExpressionTest.sol
+++ test/concrete/flowBasic/FlowExpressionTest.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: CAL
 pragma solidity ^0.8.18;
 
-import "forge-std/console.sol";
 import {Vm} from "forge-std/Test.sol";
 
 import {FlowBasicTest} from "test/abstract/FlowBasicTest.sol";
